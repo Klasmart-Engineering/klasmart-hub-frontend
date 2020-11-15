4635909ded2fb916f4dff3ef2b20713be08ec67e
Owen Delahoy <owen.delahoy@gmail.com>
2020-11-16 02:18:54 +0900

    re-enable production optimizations

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -5,7 +5,7 @@ const Visualizer = require("webpack-visualizer-plugin");
 const output_file_name = "bundle.[chunkhash].js";
 
 module.exports = {
-    mode: "development",
+    mode: "production",
     entry: {
         hubui: "./src/client-entry.tsx",
     },
