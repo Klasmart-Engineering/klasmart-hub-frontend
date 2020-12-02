7a9b58907474407e726a047d1dbe9aa625e62bcb
Shawn Lee <shawn.lee@calmid.com>
2020-12-02 21:17:11 +0900

    Updated prod deployment script

--- deploy-prod-seoul.sh
+++ deploy-prod-seoul.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 npm run build:prod
-aws s3 sync dist s3://kidsloop-hub-site
-aws cloudfront create-invalidation --paths "/*" --distribution-id E214L6N83MHUOL #hub.kidsloop.net
+aws s3 sync dist s3://kidsloop-hub-prod
+aws cloudfront create-invalidation --paths "/*" --distribution-id E1VK57W60MLYQ5 #hub.kidsloop.net
