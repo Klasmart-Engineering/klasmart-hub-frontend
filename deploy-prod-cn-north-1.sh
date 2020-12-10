befa623419854c077674f87b7ff3f6ba42d861ae
owen-delahoy <owen.delahoy@calmid.com>
2020-12-10 16:35:34 +0900

    Correction use hub bucket

--- deploy-prod-cn-north-1.sh
+++ deploy-prod-cn-north-1.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 npm i
 npm run build:prod-cn
-aws s3 sync dist s3://kidsloop-auth-cn-north-1-prod --region cn-north-1 --dryrun
+aws s3 sync dist s3://kidsloop-hub-cn-north-1-prod --region cn-north-1 --dryrun
 echo "Waiting 60s before deploying to production in cn-north-1"
 sleep 60
-aws s3 sync dist s3://kidsloop-auth-cn-north-1-prod --region cn-north-1
+aws s3 sync dist s3://kidsloop-hub-cn-north-1-prod --region cn-north-1
 aws cloudfront create-invalidation --region cn-north-1 --paths "/*" --distribution-id E3ABTCBR75Z6GO #hub.kidsloop.cn
