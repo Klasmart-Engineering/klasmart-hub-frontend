8566ccaae0b59baf105fd7bb43aa727b8314dca2
Henrik Wassdahl <henrik.wassdahl@calmid.com>
2021-01-19 18:16:27 +0900

    fix: add missing parameter

--- src/components/User/Dialog/Create.tsx
+++ src/components/User/Dialog/Create.tsx
@@ -1,20 +1,29 @@
-import React, { useEffect, useState } from "react";
-import { createStyles, makeStyles } from "@material-ui/core";
+import React,
+{
+    useEffect,
+    useState,
+} from "react";
+import {
+    createStyles,
+    makeStyles,
+} from "@material-ui/core";
 import { OrganizationMembership } from "@/types/graphQL";
 import UserDialogForm from "./Form";
 // import { useCreateUser } from "@/api/users";
-import { Dialog, useSnackbar } from "kidsloop-px";
+import {
+    Dialog,
+    useSnackbar,
+} from "kidsloop-px";
 import { useReactiveVar } from "@apollo/client";
 import { currentMembershipVar } from "@/cache";
 import { buildEmptyOrganizationMembership } from "@/utils/organizationMemberships";
 import { useCreateOrganizationMembership } from "@/api/organizationMemberships";
 
-const useStyles = makeStyles((theme) => createStyles({
-}));
+const useStyles = makeStyles((theme) => createStyles({}));
 
 interface Props {
-    open: boolean
-    onClose: (value?: OrganizationMembership) => void
+    open: boolean;
+    onClose: (value?: OrganizationMembership) => void;
 }
 
 export default function CreateUserDialog (props: Props) {
@@ -23,45 +32,52 @@ export default function CreateUserDialog (props: Props) {
         onClose,
     } = props;
     const classes = useStyles();
-    const { enqueueSnackbar } = useSnackbar()
+    const { enqueueSnackbar } = useSnackbar();
     const [ valid, setValid ] = useState(true);
+    const organization = useReactiveVar(currentMembershipVar);
     const [ newOrganizationMembership, setNewOrganizationMembership ] = useState(buildEmptyOrganizationMembership());
     const [ createOrganizationMembership, { loading: loadingCreate } ] = useCreateOrganizationMembership();
 
     useEffect(() => {
         if (!open) return;
-        setNewOrganizationMembership(buildEmptyOrganizationMembership());
-    }, [open]);
+        const membership = buildEmptyOrganizationMembership();
+        membership.organization_id = organization.organization_id;
+        setNewOrganizationMembership(membership);
+    }, [ open, organization ]);
 
     const handleCreate = async () => {
         try {
             await createOrganizationMembership(newOrganizationMembership);
             onClose(newOrganizationMembership);
-            enqueueSnackbar("User has been created succesfully", { variant: "success" });
+            enqueueSnackbar(`User has been created succesfully`, {
+                variant: `success`,
+            });
         } catch (error) {
-            enqueueSnackbar("Sorry, something went wrong, please try again", { variant: "error" });
+            enqueueSnackbar(`Sorry, something went wrong, please try again`, {
+                variant: `error`,
+            });
         }
     };
 
     return (
         <Dialog
             open={open}
-            onClose={() => onClose()}
             title="Create user"
             actions={[
                 {
-                    label: "Cancel",
-                    color: "primary",
+                    label: `Cancel`,
+                    color: `primary`,
                     onClick: () => onClose(),
                 },
                 {
-                    label: "Create",
-                    color: "primary",
+                    label: `Create`,
+                    color: `primary`,
                     loading: loadingCreate,
                     disabled: !valid,
                     onClick: handleCreate,
                 },
             ]}
+            onClose={() => onClose()}
         >
             <UserDialogForm
                 value={newOrganizationMembership}
