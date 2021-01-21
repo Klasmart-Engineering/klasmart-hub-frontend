c390a2535a4ee66bc5ea5db66870b31b0ff7d8e8
Shawn Lee <shawn.lee@calmid.com>
2021-01-21 11:58:32 +0900

    Updating deploy configs

--- deploy-beta-seoul.sh
+++ deploy-beta-seoul.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-npm run build:test
+npm run build:beta
 aws s3 sync dist s3://kidsloop-beta-hub-site
 aws cloudfront create-invalidation --paths "/*" --distribution-id E2E5VY9QII3K8T #beta-hub.kidsloop.net
--- webpack.beta.config.js
+++ webpack.beta.config.js
@@ -1,26 +1,25 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const webpack = require("webpack");
-const Visualizer = require("webpack-visualizer-plugin");
+const path = require(`path`);
+const HtmlWebpackPlugin = require(`html-webpack-plugin`);
+const webpack = require(`webpack`);
+const Visualizer = require(`webpack-visualizer-plugin`);
+const output_file_name = `bundle.[chunkhash].js`;
 
 module.exports = {
-    mode: "production",
+    mode: `development`,
     entry: {
-        hubui: "./src/client-entry.tsx",
-        record: "./src/pages/classroom/live/liveClient/entry-record.ts",
-        player: "./src/pages/classroom/live/liveClient/entry-player.ts"
+        hubui: `./src/client-entry.tsx`,
     },
     module: {
         rules: [
             {
                 test: /\.(j|t)sx?$/,
                 use: {
-                    loader: "babel-loader",
-                }
+                    loader: `babel-loader`,
+                },
             },
             {
                 test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
+                use: [ `style-loader`, `css-loader` ],
                 // use: [
                 //     {
                 //         loader: "style-loader",
@@ -37,9 +36,9 @@ module.exports = {
             {
                 test: /\.(gif|png|jpe?g|svg)$/i,
                 use: [
-                    "file-loader",
+                    `file-loader`,
                     {
-                        loader: "image-webpack-loader",
+                        loader: `image-webpack-loader`,
                         options: {
                             // mozjpeg: {
                             //     progressive: true,
@@ -50,55 +49,57 @@ module.exports = {
                             //     enabled: false,
                             // },
                             pngquant: {
-                                quality: [0.65, 0.90],
-                                speed: 4
+                                quality: [ 0.65, 0.90 ],
+                                speed: 4,
                             },
-                        }
+                        },
                     },
                 ],
             },
             {
                 test: /\.(woff|woff2|eot|ttf|otf)$/,
-                use: [
-                    "file-loader",
-                ],
+                use: [ `file-loader` ],
+            },
+            {
+                test: /\.mp4$/,
+                use: `file-loader?name=videos/[name].[ext]`,
             },
         ],
     },
     resolve: {
-        extensions: [".js", ".jsx", ".tsx", ".ts"],
+        extensions: [
+            `.js`,
+            `.jsx`,
+            `.tsx`,
+            `.ts`,
+        ],
         alias: {
-            react: path.resolve("./node_modules/react"),
-            "@": path.resolve(__dirname, "src") 
+            react: path.resolve(`./node_modules/react`),
+            "@": path.resolve(__dirname, `src`),
         },
     },
     output: {
-        filename: "[name].[chunkhash].js",
-        chunkFilename: "[name].[chunkhash].js",
-        path: path.resolve(__dirname, "dist"),
+        filename: `[name].[chunkhash].js`,
+        chunkFilename: `[name].[chunkhash].js`,
+        path: path.resolve(__dirname, `dist`),
     },
     plugins: [
         new HtmlWebpackPlugin({
-            filename: "index.html",
-            chunks: ["hubui"],
-            template: "src/index.html",
+            filename: `index.html`,
+            chunks: [ `hubui` ],
+            template: `src/index_prod.html`,
         }),
-        new HtmlWebpackPlugin({
-            filename: "player.html",
-            chunks: ["player"],
-            template: "src/pages/classroom/live/liveClient/player.html"
-        }),
-        new webpack.ProvidePlugin({
-            "fetch": "imports-loader?this=>global!exports-loader?global.fetch!whatwg-fetch"
-        }),
-        new Visualizer({ filename: "../webpack-stats.html" }),
+        // new webpack.ProvidePlugin({
+        //     "fetch": "imports-loader?this=>global!exports-loader?global.fetch!whatwg-fetch"
+        // }),
+        // new Visualizer({ filename: "../webpack-stats.html" }),
         new webpack.EnvironmentPlugin({
-            "STAGE": "beta",
-            "CN_CMS_ENDPOINT": "https://kl2-test.kidsloop.net/",
-            "API_ENDPOINT": "https://api.kidsloop.net/",
-            "AUTH_ENDPOINT": "https://auth.kidsloop.net/",
-            "LIVE_ENDPOINT": "https://live.kidsloop.net/",
-            "COOKIE_DOMAIN": "kidsloop.net",
-        })
+            STAGE: `prod`,
+            CN_CMS_ENDPOINT: `https://kl2-test.kidsloop.net/`,
+            API_ENDPOINT: `https://api.kidsloop.net/`,
+            AUTH_ENDPOINT: `https://auth.kidsloop.net/`,
+            LIVE_ENDPOINT: `https://live.kidsloop.net/`,
+            COOKIE_DOMAIN: `kidsloop.net`,
+        }),
     ],
 };
