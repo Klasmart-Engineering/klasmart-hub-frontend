a044bf6c8c50cb2752f9953a30a4b94ef06dcf74
Henrik Wassdahl <henrik.wassdahl@calmid.com>
2021-04-05 10:16:20 +0900

    fixed scrolling issue

--- src/layout.tsx
+++ src/layout.tsx
@@ -16,7 +16,6 @@ const useStyles = makeStyles((theme: Theme) => createStyles({
     },
     content: {
         flex: 1,
-        overflow: `hidden`,
     },
     navMainContainer: {
         display: `flex`,
