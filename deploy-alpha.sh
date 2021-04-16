f886be88d9464dbc13edba976ec87079c2a093be
Shawn Lee <shawn.lee@calmid.com>
2021-04-16 15:39:32 +0900

    Adjustments to deployment scripts

--- deploy-alpha.sh
+++ deploy-alpha.sh
@@ -6,6 +6,6 @@ API_ENDPOINT="https://api.alpha.kidsloop.net/" \
 AUTH_ENDPOINT="https://auth.alpha.kidsloop.net/" \
 LIVE_ENDPOINT="https://live.alpha.kidsloop.net/" \
 COOKIE_DOMAIN="alpha.kidsloop.net" \
-npm run build:prod
+npm run build:alpha
 aws s3 sync dist s3://kidsloop-hub-alpha-dynamic-louse/alpha/latest
 aws cloudfront create-invalidation --paths "/*" --distribution-id E15G7HP1IMB2DX #hub.alpha.kidsloop.net
--- deploy-prod-seoul.sh
+++ deploy-prod-seoul.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 npm run build:prod
+aws s3 sync s3://kidsloop-hub-prod/ backup
 aws s3 sync dist s3://kidsloop-hub-prod
 aws cloudfront create-invalidation --paths "/*" --distribution-id E1VK57W60MLYQ5 #hub.kidsloop.net
