dd9cb54204878f4c6d9d2427ae9ad77fb15eee1b
Henrik Wassdahl <henrik.wassdahl@calmid.com>
2021-05-13 11:31:41 +0900

    remove unused scripts

--- deploy-test-seoul.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-npm run build:test
-aws s3 sync dist s3://kidsloop-test-hub-site
-aws cloudfront create-invalidation --paths "/*" --distribution-id E1B9GOHPCRYKDS #test-hub.kidsloop.net
