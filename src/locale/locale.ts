09304e0d4bc67c756c41d4fadacc32a740826d06
Henrik Wassdahl <henrik.wassdahl@calmid.com>
2021-06-22 01:52:20 +0000

    Merged in lokalise-setup (pull request #239)
    Lokalise setup
    
    * remove variables from locale files
    
    * convert locale files from ts to json
    
    * delete unused file
    
    Approved-by: Malcolm McConaghy

--- src/locale/locale.ts
+++ src/locale/locale.ts
@@ -1,48 +1,75 @@
-import { createIntl, createIntlCache } from "react-intl";
-import english from "./en";
-import korean from "./ko";
-import chinese from "./zh_cn";
-import vietnamese from "./vi";
-import indonesian from "./id";
+import english from "./en.json";
+import indonesian from "./id.json";
+import korean from "./ko.json";
+import vietnamese from "./vi.json";
+import chinese from "./zh_CN.json";
 import { Language } from "kidsloop-px/dist/types/components/LanguageSelect";
+import {
+    createIntl,
+    createIntlCache,
+} from "react-intl";
 
-export const localeCodes = ["en-US", "ko", "zh-CN", "vi", "id"];
+export const localeCodes = [
+    `en-US`,
+    `ko`,
+    `zh-CN`,
+    `vi`,
+    `id`,
+];
 export const LANGUAGES_LABEL: Language[] = [
     {
-        code: "en-US",
-        text: "English",
+        code: `en-US`,
+        text: `English`,
     },
     {
-        code: "ko",
-        text: "한국어",
+        code: `ko`,
+        text: `한국어`,
     },
     {
-        code: "zh-CN",
-        text: "汉语 (简体)",
+        code: `zh-CN`,
+        text: `汉语 (简体)`,
     },
     {
-        code: "vi",
-        text: "Tiếng Việt",
+        code: `vi`,
+        text: `Tiếng Việt`,
     },
     {
-        code: "id",
-        text: "bahasa Indonesia",
+        code: `id`,
+        text: `bahasa Indonesia`,
     },
 ];
 
 const intlCache = createIntlCache();
-export const fallbackLocale = createIntl({ locale: "en", messages: english }, intlCache);
-export function getIntl(locale: string) {
+export const fallbackLocale = createIntl({
+    locale: `en`,
+    messages: english,
+}, intlCache);
+export function getIntl (locale: string) {
     switch (locale) {
-        case "en":
-            return createIntl({ locale: "en-US", messages: english }, intlCache);
-        case "ko":
-            return createIntl({ locale: "ko", messages: korean }, intlCache);
-        case "zh-CN":
-            return createIntl({ locale: "zh-CN", messages: chinese }, intlCache);
-        case "vi":
-            return createIntl({ locale: "vi", messages: vietnamese }, intlCache);
-        case "id":
-            return createIntl({ locale: "id", messages: indonesian }, intlCache);
+    case `en`:
+        return createIntl({
+            locale: `en-US`,
+            messages: english,
+        }, intlCache);
+    case `ko`:
+        return createIntl({
+            locale: `ko`,
+            messages: korean,
+        }, intlCache);
+    case `zh-CN`:
+        return createIntl({
+            locale: `zh-CN`,
+            messages: chinese,
+        }, intlCache);
+    case `vi`:
+        return createIntl({
+            locale: `vi`,
+            messages: vietnamese,
+        }, intlCache);
+    case `id`:
+        return createIntl({
+            locale: `id`,
+            messages: indonesian,
+        }, intlCache);
     }
 }
