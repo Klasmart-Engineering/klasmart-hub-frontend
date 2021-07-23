a1a9553606853eb9d4e3abae974508e645a196ee
Jean Georges <jean.georges@calmid.com>
2021-07-23 02:29:48 +0000

    Merged in feat/build-envs (pull request #285)
    update build envs, remove unused config
    
    * change order of fields
    
    * update build envs, remove unused config
    
    
    Approved-by: Henrik Wassdahl

--- deploy-prod-in.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-npm ci --no-progress
-npm audit fix
-npm run build:prod-in
-aws s3 sync dist s3://klindia-prod-hub/latest
-aws s3 sync dist s3://klindia-prod-hub/latest --delete
-aws cloudfront create-invalidation --paths "/*" --distribution-id E1VSI2XC8592JJ #hub-prod.kidsloop.in
