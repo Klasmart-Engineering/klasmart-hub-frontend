0f282f8658a352108fe3fc88964cb954c687700f
Stephen Palmer <stephen.palmer@kidsloop.live>
2022-01-20 17:33:23 +0000

    Merged in dashboard-api-provider-access-refresh (pull request #564)
    fix: adding a refresh to the dashboard api provider to update access tokens
    
    * fix: adding a refresh to the dashboard api provider to update access tokens
    
    * fixes based on review comments
    
    * fix: dashboard api retry on rejected
    
    * breaking the api provider
    
    * fix: api provider access refresh - WIP
    
    * fix: updated api client to include a refresh timeout
    
    * fix: updated function name to better match its purpose
    
    * fix: added proxy back into webpack config
    
    
    Approved-by: Ricardo Barata
    Approved-by: Malcolm McConaghy
    Approved-by: Ismael Perez

--- src/providers/ReportsApiClient.tsx
+++ src/providers/ReportsApiClient.tsx
@@ -1,7 +1,17 @@
+import { authClient } from "@/api/auth/client";
 import { getReportsEndpoint } from "@/config";
 import { ReportsApiClientProvider as KLReportsApiClientProvider } from "@kidsloop/reports-api-client";
-import React from "react";
+import { AxiosError } from "axios";
+import React,
+{
+    useCallback,
+    useEffect,
+    useState,
+} from "react";
 import { useCookies } from "react-cookie";
+
+const AUTH_HEADER = `authorization`;
+const ACCESS_TOKEN_COOKIE = `access`;
 
 interface Props {
     children: React.ReactNode;
@@ -10,22 +20,36 @@ interface Props {
 export default function ReportsApiClientProvider (props: Props) {
     const { children } = props;
     const reportsServiceEndpoint = getReportsEndpoint();
-    const [ access ] = useCookies([ `access` ]);
+    const [ cookies ] = useCookies([ ACCESS_TOKEN_COOKIE ]);
+    const [ currentDate, setCurrentDate ] = useState(Date.now());
 
     const STALE_TIME = 60 * 1000; // 60 seconds
+    const REQUEST_RETRY_MAX_COUNT = 3; // 3
+
+    useEffect(() => {
+        let timeTilExpiry = STALE_TIME;
+        const getTokenExpiry = async () => {
+            const { exp } = await authClient.refreshToken();
+            if (exp) timeTilExpiry = exp - (currentDate / 1000);
+        };
+        getTokenExpiry();
+        const timeOut = setTimeout(() => setCurrentDate(Date.now()), timeTilExpiry);
+        return () => clearTimeout(timeOut);
+    }, [ currentDate ]);
 
     return (
         <KLReportsApiClientProvider
             config={{
                 baseURL: reportsServiceEndpoint,
                 headers: {
-                    authorization: access.access,
+                    [AUTH_HEADER]: cookies.access,
                 },
             }}
             queryOptions={{
                 defaultOptions: {
                     queries: {
                         staleTime: STALE_TIME,
+                        retry: REQUEST_RETRY_MAX_COUNT,
                     },
                 },
             }}
