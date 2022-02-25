d863e80345d7313ed46a316fe0f647922f214777
Ricardo Barata <ricardo.barata@kidsloop.live>
2022-02-25 13:35:07 +0000

    fix: comment out the marked as inactive and reactivate buttons
    Merged in fix/disable-user-reactivate (pull request #672)
    
    Approved-by: Richard Sommerville

--- src/operations/mutations/reactivateUser.tsx
+++ src/operations/mutations/reactivateUser.tsx
@@ -1,11 +1,12 @@
 import { gql } from "@apollo/client";
 
 export const REACTIVATE_USER_IN_ORGANIZATION = gql`
-    mutation reactivateUserInOrganization($user_ids: [ID!], $organization_id: ID!) {
-        reactivateUsersFromOrganizations(userIds: $user_ids, organizationId: $organization_id) {
-            organizations {
-                id
-            }
+mutation reactivateUserInOrganization($user_ids: [ID!]!, $organization_id: ID!) {
+    reactivateUsersFromOrganizations(
+        input: [{userIds: $user_ids, organizationId: $organization_id}]
+    ) {
+        organizations {
+            id
         }
     }
-`;
+}`;
