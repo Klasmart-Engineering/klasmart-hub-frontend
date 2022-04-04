b1fb533442f4fd9143dacdbc4f2be1e322890016
Henrik Wassdahl <99242397+henrik-wassdahl-kidsloop@users.noreply.github.com>
2022-04-04 20:03:36 +0900

    feat(feature-flag): add default values (#22)

--- src/feature-flag/utils.ts
+++ src/feature-flag/utils.ts
@@ -1,11 +1,19 @@
 import { useFlags } from "launchdarkly-react-client-sdk";
 
 export type FeatureFlags = {
-      studentWidgetAdaptiveLearning: boolean;
-      studentWidgetAdaptiveLearningJourney: boolean;
+    studentWidgetAdaptiveLearning: boolean;
+    studentWidgetAdaptiveLearningJourney: boolean;
 }
 
+const defaultFlags: FeatureFlags = {
+    studentWidgetAdaptiveLearning: false,
+    studentWidgetAdaptiveLearningJourney: false,
+};
+
 export const useFeatureFlags = () => {
-    const flags = useFlags();
-    return flags as FeatureFlags;
+    const flags = useFlags() as FeatureFlags;
+    return {
+        ...defaultFlags,
+        ...flags,
+    } as FeatureFlags;
 };
