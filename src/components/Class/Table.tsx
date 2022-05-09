8e1bf5e0ecce4999f7371e32c05048f47d5b562f
stephen-palmer-kidsloop <98025533+stephen-palmer-kidsloop@users.noreply.github.com>
2022-05-09 17:22:05 +0100

    fix: upgraded px to table fix version (#98)

--- src/components/Class/Table.tsx
+++ src/components/Class/Table.tsx
@@ -400,7 +400,7 @@ export default function ClassesTable (props: Props) {
                 },
             ],
         },
-        ...false ? [ academicTermFilter ] : [],
+        ...showAcademicTermFilter ? [ academicTermFilter ] : [],
         {
             id: `status`,
             label: intl.formatMessage({
