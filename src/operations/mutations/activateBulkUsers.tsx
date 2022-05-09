5a9dc2ad53131aa0f9696031590e8a13517dba22
Ruchika Bharati <ruchika@chrysalis.world>
2022-05-09 15:31:12 +0530

    feat: add integration for bulk inactivate activate user

---
+++ src/operations/mutations/activateBulkUsers.tsx
@@ -0,0 +1,12 @@
+import { gql } from "@apollo/client";
+
+export const REACTIVATE_BULK_USERS_IN_ORGANIZATION = gql`
+mutation reactivateAllUserInOrganization($userIds: [ID!]!, $organizationId: ID!) {
+    reactivateUsersFromOrganizations(
+        input: [{
+            organizationId: $organizationId
+            userIds: $userIds, 
+        }]
+    )
+}
+`;
