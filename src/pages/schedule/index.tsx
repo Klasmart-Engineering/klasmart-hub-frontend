6b0430a921f13ba6d795f5cf0ab08a9ccb9f39f8
Henrik Wassdahl <99242397+henrik-wassdahl-kidsloop@users.noreply.github.com>
2022-05-26 09:58:51 +0900

    feat: remove schedule iframe (#122)
    (cherry picked from commit bd27f6aa92013b7ce505184b6707ea9c980b57aa)

--- src/pages/schedule/index.tsx
+++ src/pages/schedule/index.tsx
@@ -1,20 +1,7 @@
 import LoadingPage from "@/components/Common/LoadingPage";
-import { getCmsSiteEndpoint } from "@/config";
-import { useFeatureFlags } from "@/feature-flag/utils";
 import { useCurrentOrganization } from "@/store/organizationMemberships";
-import {
-    createStyles,
-    makeStyles,
-} from "@mui/styles";
 import React,
 { Suspense } from "react";
-
-const useStyles = makeStyles((theme) => createStyles({
-    root: {
-        width: `100%`,
-        height: `100%`,
-    },
-}));
 
 interface Props {
 }
@@ -24,28 +11,16 @@ interface Props {
 const SchedulePageMFE = React.lazy(() => import(`schedule/Schedule`));
 
 export default function SchedulePage (props: Props) {
-    const classes = useStyles();
     const currentOrganization = useCurrentOrganization();
     const organizationId = currentOrganization?.id ?? ``;
-    const { showScheduleMicroFrontend } = useFeatureFlags();
-
-    if (showScheduleMicroFrontend) {
-        return (
-            <Suspense
-                fallback={(
-                    <LoadingPage />
-                )}
-            >
-                <SchedulePageMFE organization_id={currentOrganization?.id} />
-            </Suspense>
-        );
-    }
 
     return (
-        <iframe
-            src={`${getCmsSiteEndpoint()}?org_id=${organizationId}#/schedule/calendar`}
-            frameBorder="0"
-            className={classes.root}
-        />
+        <Suspense
+            fallback={(
+                <LoadingPage />
+            )}
+        >
+            <SchedulePageMFE organization_id={organizationId} />
+        </Suspense>
     );
 }
