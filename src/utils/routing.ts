e2004a82dc49d3b24e7b803e8812c7396703170f
sankar-kandasamy-kidsloop <102366410+sankar-kandasamy-kidsloop@users.noreply.github.com>
2022-06-27 17:57:36 +0530

    fix: remove auth logout route enabled feature flag from hub frontend (#163)
    * fix: remove auth logout route enabled feature flag
    
    * fix: logout url changed

--- src/utils/routing.ts
+++ src/utils/routing.ts
@@ -1,17 +1,14 @@
 import { AuthClient } from "@/api/auth/client";
 
 interface RedirectToAuthOptions {
-    withParams?: boolean;
+  withParams?: boolean;
 }
 
 export const redirectToAuth = (options?: RedirectToAuthOptions) => {
     const params = new URLSearchParams({
-        continue: options?.withParams ? window.location.href : window.location.origin,
+        continue: options?.withParams
+            ? window.location.href
+            : window.location.origin,
     });
-
-    if (process.env.AUTH_LOGOUT_ROUTE_ENABLED === `true`) {
-        window.location.href = `${AuthClient.baseURL}logout?${params.toString()}`;
-    } else {
-        window.location.href = `${AuthClient.baseURL}?${params.toString()}#/`;
-    }
+    window.location.href = `${AuthClient.baseURL}logout?${params.toString()}`;
 };
