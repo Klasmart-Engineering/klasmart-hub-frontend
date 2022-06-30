8749603106ad2e93247b961e5af57a5841bc79e1
ruchika-bharati-kidsloop <103484441+ruchika-bharati-kidsloop@users.noreply.github.com>
2022-06-30 11:58:06 +0530

    feat: display warning for delete user by admin level (#130)
    * feat: display warning for delete user by admin level
    
    * feat: display warning fo delete user
    
    * fix: "display warning for delete user by admin level"
    
    * feat: "display warning fro delete user by admin level"
    
    * feat: "display warning for delete user by admin level"
    
    * feat: display warning for delete user by admin level
    
    * feat: display warning for delete user
    
    * fix: display warning for delete user by admin level
    
    * feat: display warning for delete user
    
    * feat: display warning for delete user by admin level
    
    * feat: display warning for delete user
    
    * fix: display warning for delete user
    
    Co-authored-by: pradrai <pradeep.rai@chrysalis.world>
    Co-authored-by: Pradeep Kumar Rai <102364897+pradeep-rai-kidsloop@users.noreply.github.com>

---
+++ src/components/User/Warning/GeneratedData.tsx
@@ -0,0 +1,193 @@
+import KidsloopLogo from "@/assets/img/kidsloop.svg";
+import { Typography } from "@mui/material";
+import { Theme } from '@mui/material/styles';
+import {
+    createStyles,
+    makeStyles,
+} from '@mui/styles';
+import { FormattedMessage } from "react-intl";
+
+const useStyles = makeStyles((theme: Theme) => {
+    return createStyles({
+        root: {
+            margin: `auto`,
+            width: `80%`,
+            maxWidth: 600,
+            [theme.breakpoints.down(`sm`)]: {
+                width: `100%`,
+                padding: theme.spacing(3),
+            },
+        },
+        container: {
+            marginTop: theme.spacing(3),
+            marginBottom: theme.spacing(6),
+            display: `flex`,
+            justifyContent: `space-between`,
+        },
+        header: {
+            alignSelf: `center`,
+        },
+        kidsloopLogo: {
+            height: `2.5rem`,
+            [theme.breakpoints.down(`sm`)]: {
+                height: `2rem`,
+            },
+        },
+        content: {
+            marginTop: theme.spacing(1),
+            marginBottom: theme.spacing(4),
+        },
+    });
+});
+
+export default function GeneratedData () {
+    const classes = useStyles();
+    return (
+        <div className={classes.root}>
+            <div className={classes.container}>
+                <Typography
+                    variant="h4"
+                    component="h2"
+                    className={classes.header}
+                >
+                    <FormattedMessage
+                        id="generatedData.userDeletion.label"
+                    />
+                </Typography>
+                <img
+                    className={classes.kidsloopLogo}
+                    alt="KidsLoop Logo - Home"
+                    src={KidsloopLogo}
+                />
+            </div>
+            <Typography
+                variant="body2"
+                component="p"
+                className={classes.content}
+            >
+                <FormattedMessage
+                    id="generatedData.userDeletion.description"
+                />
+            </Typography>
+            <Typography
+                variant="h5"
+                component="h2"
+            >
+                <FormattedMessage
+                    id="generatedData.academicData.label"
+                />
+            </Typography>
+            <Typography
+                variant="body2"
+                component="p"
+                className={classes.content}
+            >
+                <FormattedMessage
+                    id="generatedData.academicData.description"
+                />
+            </Typography>
+            <Typography
+                variant="h5"
+                component="h2"
+            >
+                <FormattedMessage
+                    id="generatedData.assessmentData.label"
+                />
+            </Typography>
+            <Typography
+                variant="body2"
+                component="p"
+                className={classes.content}
+            >
+                <FormattedMessage
+                    id="generatedData.assessmentData.description"
+                />
+            </Typography>
+            <Typography
+                variant="h5"
+                component="h2"
+            >
+                <FormattedMessage
+                    id="generatedData.classes.label"
+                />
+            </Typography>
+            <Typography
+                variant="body2"
+                component="p"
+                className={classes.content}
+            >
+                <FormattedMessage
+                    id="generatedData.classes.description"
+                />
+            </Typography>
+            <Typography
+                variant="h5"
+                component="h2"
+            >
+                <FormattedMessage
+                    id="generatedData.contentLibrary.label"
+                />
+            </Typography>
+            <Typography
+                variant="body2"
+                component="p"
+                className={classes.content}
+            >
+                <FormattedMessage
+                    id="generatedData.contentLibrary.description"
+                />
+            </Typography>
+            <Typography
+                variant="h5"
+                component="h2"
+            >
+                <FormattedMessage
+                    id="generatedData.live.label"
+                />
+            </Typography>
+            <Typography
+                variant="body2"
+                component="p"
+                className={classes.content}
+            >
+                <FormattedMessage
+                    id="generatedData.live.description"
+                />
+            </Typography>
+            <Typography
+                variant="h5"
+                component="h2"
+            >
+                <FormattedMessage
+                    id="generatedData.reportData.label"
+                />
+            </Typography>
+            <Typography
+                variant="body2"
+                component="p"
+                className={classes.content}
+            >
+                <FormattedMessage
+                    id="generatedData.reportData.description"
+                />
+            </Typography>
+            <Typography
+                variant="h5"
+                component="h2"
+            >
+                <FormattedMessage
+                    id="generatedData.scheduleData.label"
+                />
+            </Typography>
+            <Typography
+                variant="body2"
+                component="p"
+                className={classes.content}
+            >
+                <FormattedMessage
+                    id="generatedData.scheduleData.description"
+                />
+            </Typography>
+        </div>
+    );
+}
