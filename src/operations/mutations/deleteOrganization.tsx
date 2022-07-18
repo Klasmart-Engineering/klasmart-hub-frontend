fe3b3e5a2153bf502926123ced8da03b1c0718a5
Asif-Tahashildar-Kidsloop <105648457+Asif-Tahashildar-Kidsloop@users.noreply.github.com>
2022-07-18 18:20:43 +0530

    fix: update mutation organization

--- src/operations/mutations/deleteOrganization.tsx
+++ src/operations/mutations/deleteOrganization.tsx
@@ -1,7 +1,7 @@
 import { gql } from "@apollo/client";
 
 export const DELETE_ORGANIZATION = gql`
-    mutation organization($organization_id: ID!) {
+    mutation deleteOrganizations($organization_id: ID!) {
         deleteOrganizations(input: [{id: $organization_id}]) {
             organizations {
               id
